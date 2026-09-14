62f6c82073decb55ed9fd3bed91d287df5c5f166
Dhanyashree <dhanyashree316@gmail.com>
2026-09-14 10:54:40 +0530

    Add remaining solution code

---
+++ Arrays/Left and Right Sum Differences.py
@@ -0,0 +1,22 @@
+# 2574. Left and Right Sum Differences
+
+class Solution:
+    def leftRightDifference(self, nums):
+
+        n = len(nums)
+        answer = [0] * n
+
+        total_sum = sum(nums)
+        left_sum = 0
+
+        for i in range(n):
+
+            total_sum -= nums[i]   # now total_sum = rightSum[i]
+
+            right_sum = total_sum
+
+            answer[i] = abs(left_sum - right_sum)
+
+            left_sum += nums[i]
+
+        return answer
