66ac90c362eff079d755f35e575a2835d62ce5ca
Dhanyashree <dhanyashree316@gmail.com>
2026-09-17 20:44:01 +0530

    Update solutions

--- Arrays/Maximum Ice Cream Bars.py
+++ Arrays/Maximum Ice Cream Bars.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  # 1833. Maximum Ice Cream Bars
 
 class Solution:
@@ -24,3 +25,31 @@ class Solution:
                 break
 
         return count
+=======
+# 1833. Maximum Ice Cream Bars
+
+class Solution:
+    def maxIceCream(self, costs, coins):
+        max_cost = max(costs)
+
+        freq = [0] * (max_cost + 1)
+
+        for cost in costs:
+            freq[cost] += 1
+
+        count = 0
+
+        for cost in range(1, max_cost + 1):
+            if freq[cost] == 0:
+                continue
+
+            can_buy = min(freq[cost], coins // cost)
+
+            count += can_buy
+            coins -= can_buy * cost
+
+            if coins < cost:
+                break
+
+        return count
+>>>>>>> ee97e88 (Update solutions)
