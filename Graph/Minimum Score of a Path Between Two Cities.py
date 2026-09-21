a26a0b7265c5855dd09cb6e6c687ed09f2895988
Dhanyashree S <dhanyashree316@gmail.com>
2026-09-21 10:30:09 +0530

    changes

---
+++ Graph/Minimum Score of a Path Between Two Cities.py
@@ -0,0 +1,28 @@
+# 2492. Minimum Score of a Path Between Two Cities
+
+from collections import defaultdict, deque
+
+class Solution:
+    def minScore(self, n: int, roads: list[list[int]]) -> int:
+        graph = defaultdict(list)
+
+        for u, v, d in roads:
+            graph[u].append((v, d))
+            graph[v].append((u, d))
+
+        visited = set()
+        q = deque([1])
+        visited.add(1)
+
+        ans = float('inf')
+
+        while q:
+            node = q.popleft()
+
+            for nei, dist in graph[node]:
+                ans = min(ans, dist)
+                if nei not in visited:
+                    visited.add(nei)
+                    q.append(nei)
+
+        return ans
