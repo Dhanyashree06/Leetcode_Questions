26b5088b032eb6bf47f565c0e2f835c6b31e3ec8
Dhanyashree <dhanyashree316@gmail.com>
2026-09-23 21:15:40 +0530

    Add graph path existence solutions

---
+++ Graph/Path Existence Queries in a Graph I.py
@@ -0,0 +1,22 @@
+# 3532. Path Existence Queries in a Graph I
+
+
+from typing import List
+
+class Solution:
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        comp = [0] * n
+
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] <= maxDiff:
+                comp[i] = comp[i - 1]
+            else:
+                comp[i] = comp[i - 1] + 1
+
+        ans = []
+        for u, v in queries:
+            ans.append(comp[u] == comp[v])
+
+        return ans
+
+
---
+++ Graph/Path Existence Queries in a Graph II.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+class Solution {
+    public int[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[][] pairs = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            pairs[i][0] = nums[i];
+            pairs[i][1] = i;
+        }
+
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+
+        final int LOG = 20;
+        int[][] f = new int[n][LOG];
+
+        int r = n - 1;
+        for (int l = n - 1; l >= 0; l--) {
+            while (pairs[r][0] - pairs[l][0] > maxDiff) {
+                r--;
+            }
+
+            int u = pairs[l][1];
+            int v = pairs[r][1];
+
+            f[u][0] = v;
+            for (int k = 1; k < LOG; k++) {
+                f[u][k] = f[f[u][k - 1]][k - 1];
+            }
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int t = 0; t < queries.length; t++) {
+            int u = queries[t][0];
+            int v = queries[t][1];
+
+            if (nums[u] > nums[v]) {
+                int tmp = u;
+                u = v;
+                v = tmp;
+            }
+
+            if (u == v) {
+                ans[t] = 0;
+                continue;
+            }
+
+            if (nums[u] == nums[v]) {
+                ans[t] = 1;
+                continue;
+            }
+
+            int dist = 0;
+
+            for (int k = LOG - 1; k >= 0; k--) {
+                if (nums[f[u][k]] < nums[v]) {
+                    dist += 1 << k;
+                    u = f[u][k];
+                }
+            }
+
+            if (nums[f[u][0]] < nums[v]) {
+                ans[t] = -1;
+            } else {
+                ans[t] = dist + 1;
+            }
+        }
+
+        return ans;
+    }
+}
